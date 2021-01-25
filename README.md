5e8ce5ac8912de8cffc442778bd27962cb8fdd61
Aeaya <77257794+Aeaya@users.noreply.github.com>
2021-01-25 18:06:55 +0800

    Update and rename README.md to websike.php

--- README.md
+++
@@ -1,1 +0,0 @@
-# weebsike9c
---
+++ websike.php
@@ -0,0 +1,17 @@
+<html>
+<head><title> Group 29C Home Page </title></head>
+<body>
+<body bgcolor= "gray">
+<font face= "", color= "black", size= "15"> <br><br><br>
+<marquee direction= "right"> Tomorrow is </marquee></font>
+<font face= "Yu Gothic UI", color= "black", size= "15">
+<marquee> A Stranger </marquee></font> <br><br>
+<p align= "center">
+<img src= "corrupt.jpg", width= "537", length= "403"><br><br><br><br><br><br>
+</p>
+<font face= "helvetica", color= "black", size= "3">
+<p align= "center">
+<i> By: Abano, Abanilla, Cauton, Villanueva, Hecole </i>
+
+</body>
+</html>
